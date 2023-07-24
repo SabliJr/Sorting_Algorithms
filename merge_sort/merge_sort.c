d8eb0fc0834c6397a783a2b68bba75cf5bd7c78c
Sabli Jr <sablijr@yahoo.com>
2023-07-24 20:32:01 +0200

    fixed merage sorting

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,62 +1,69 @@
 #include <stdio.h>
 
-void merge_sort(int a[], int start, int end);
-void megre_func(int a[], int left, int middle, int end);
+void sorting_ele(int a[], int start, int end, int arr_len);
+void merging_elements(int a[], int start, int middle, int end, int arr_len);
 
-int main() {
 
-  int arr[] = {-2, 0, 6, 19, 38, -5, 47, 68, 4};
+int main(int argc, char *argv[]) {
+
+  int arr[] = {16, 3, -5, 23, 44, 18, 39, 76, 2};
   int arr_size = sizeof(arr) / sizeof(arr[0]);
 
-  merge_sort(arr, 0, arr_size - 1);
-  printf("The array size %d\n", arr_size);
+  sorting_ele(arr, 0, arr_size - 1, arr_size);
+
+  //Printing out the sorted array;
+  for (int i = 0; i < arr_size; i++)
+    printf("%d ", arr[i]);
 
   return 0;
 };
 
-void merge_sort(int a[], int start, int end) {
-  
+//Sorting the elements;
+void sorting_ele(int a[], int start, int end, int arr_len) {
   if (start < end) {
     int middle = (start + end) / 2;
-    merge_sort(a, start, middle);
-    merge_sort(a, middle + 1, end);
+    sorting_ele(a, start, middle, arr_len);
+    sorting_ele(a, middle + 1, end, arr_len);
 
-    megre_func(a, start, middle, end);
+    merging_elements(a, start, middle, end, arr_len);
   };
 };
 
-void megre_func(int a[], int l, int m, int end) {
+//Merging the elements togther;
+void merging_elements(int a[], int start, int middle, int end, int arr_len) {
   int i, j, k;
-  int temp[8];
+  int curr[arr_len];
 
-  i = l;
-  j = m + 1;
-  k = l;
+  i = start;
+  j = middle + 1;
+  k = start;
 
-  while(i <= m && j <= end) {
+  while (i <= middle && j <= end) {
     if (a[i] <= a[j]) {
-      temp[k] = a[i];
-      k++;
+      curr[k] = a[i];
       i++;
-    } else {
-      temp[k] = a[j];
       k++;
+    } else {
+      curr[k] = a[j];
       j++;
+      k++;
     };
   };
 
-  while(i <= m) {
-    temp[k] = a[i];
-    k++;
+  while (i <= middle) {
+    curr[k] = a[i];
     i++;
+    k++;
   };
 
   while (j <= end) {
-    temp[k] = a[j];
-    k++;
+    curr[k] = a[j];
     j++;
+    k++;
   };
 
-  for (int p = l; p <= end; p++)
-    printf("%d", temp[p]);
+  //Swaping the sorted array with the unsorted one;
+  for(int p = start; p <= end; p++)
+    a[p] = curr[p];
 };
+
