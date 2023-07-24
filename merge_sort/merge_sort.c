954acc812d1d59fed12656afe28a8890e8d83dd4
Sabli Jr <sablijr@yahoo.com>
2023-07-24 18:08:15 +0200

    failed merage sorting

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void merage_sort(int a[], int start, int end);
+void merge_sort(int a[], int start, int end);
 void megre_func(int a[], int left, int middle, int end);
 
 int main() {
@@ -8,18 +8,18 @@ int main() {
   int arr[] = {-2, 0, 6, 19, 38, -5, 47, 68, 4};
   int arr_size = sizeof(arr) / sizeof(arr[0]);
 
-  merage_sort(arr, 0, arr_size - 1);
+  merge_sort(arr, 0, arr_size - 1);
   printf("The array size %d\n", arr_size);
 
   return 0;
 };
 
-void merage_sort(int a[], int start, int end) {
+void merge_sort(int a[], int start, int end) {
   
   if (start < end) {
     int middle = (start + end) / 2;
-    merage_sort(a, start, middle);
-    merage_sort(a, middle + 1, end);
+    merge_sort(a, start, middle);
+    merge_sort(a, middle + 1, end);
 
     megre_func(a, start, middle, end);
   };
