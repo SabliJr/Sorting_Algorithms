f72e5e4b1fac79df68993b136c5b4a934ee2c752
Sabli Jr <sablijr@yahoo.com>
2023-07-25 13:52:35 +0200

    Selection sorting algorithm, while loop version

--- Selection_sort/selection_sort.c
+++ Selection_sort/selection_sort.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   //sort_selectively(arr, arr_size);
   sort_with_while(arr, arr_size);
 
-
+  //Print out the array
   for(int u = 0; u < arr_size; u++)
     printf("%d ", arr[u]);
 
