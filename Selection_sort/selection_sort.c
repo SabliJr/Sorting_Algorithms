df5ea916e6f3b1c257d1c0c593fb9c8ec62c0f7e
Sabli Jr <sablijr@yahoo.com>
2023-07-25 13:48:39 +0200

    Selection sorting algorithm, while loop version

--- Selection_sort/selection_sort.c
+++ Selection_sort/selection_sort.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 void sort_selectively(int a[], int length);
+void sort_with_while(int arr[], int len);
 
 int main(int argc, char *argv[]) {
 
   int arr[] = {5, 29, 13, 40, 18, 36, -1, 7, 0, -6, 22};
   int arr_size = sizeof(arr) / sizeof(arr[0]);
 
-  sort_selectively(arr, arr_size);
+  //sort_selectively(arr, arr_size);
+  sort_with_while(arr, arr_size);
+
 
   for(int u = 0; u < arr_size; u++)
     printf("%d ", arr[u]);
@@ -15,6 +18,7 @@ int main(int argc, char *argv[]) {
   return 0;
 };
 
+//Sorting with for loop;
 void sort_selectively(int a[], int length) {
   for(int i = 0; i < length - 1; i++) {
     int min_position = i;
@@ -29,4 +33,28 @@ void sort_selectively(int a[], int length) {
         a[min_position] = temp;
       }
   }
-}
+};
+
+//Sorting with while loop;
+void sort_with_while(int arr[], int len) {
+  int i = 0;
+  while (i < len - 1) {
+    int min_index = i;
+    int j = i + 1;
+    
+    while (j < len) {
+      if (arr[j] < arr[min_index]) 
+        min_index = j;
+
+      j++;
+    };
+
+    if (min_index != i) {
+      int temp = arr[i];
+      arr[i] = arr[min_index];
+      arr[min_index] = temp;
+    }
+
+    i++;
+  };
+};
